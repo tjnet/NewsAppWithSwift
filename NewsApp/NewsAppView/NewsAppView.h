dc9d269e81b52dd637c67c88ea11d833b091ff18
tjnet <rsa14631@nifty.com>
2016-01-22 01:05:22 +0900

    add tests of view

---
+++ NewsApp/NewsAppView/NewsAppView.h
@@ -0,0 +1,19 @@
+//
+//  NewsAppView.h
+//  NewsAppView
+//
+//  Created by TanakaJun on 2016/01/22.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewsAppView.
+FOUNDATION_EXPORT double NewsAppViewVersionNumber;
+
+//! Project version string for NewsAppView.
+FOUNDATION_EXPORT const unsigned char NewsAppViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewsAppView/PublicHeader.h>
+
+
