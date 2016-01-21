1e2f722e8a3a07f16f051435a41361f7ebc2b5d5
tjnet <rsa14631@nifty.com>
2016-01-22 00:54:04 +0900

    add tests of ViewModel

---
+++ NewsApp/NewsAppViewModel/NewsAppViewModel.h
@@ -0,0 +1,20 @@
+//
+//  NewsAppViewModel.h
+//  NewsAppViewModel
+//
+//  Created by TanakaJun on 2016/01/22.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewsAppViewModel.
+FOUNDATION_EXPORT double NewsAppViewModelVersionNumber;
+
+//! Project version string for NewsAppViewModel.
+FOUNDATION_EXPORT const unsigned char NewsAppViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewsAppViewModel/PublicHeader.h>
+
+
+
