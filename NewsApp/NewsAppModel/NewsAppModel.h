ec26de77957d3d45e41262375d163f4efa7063a5
tjnet <rsa14631@nifty.com>
2016-01-22 00:40:49 +0900

    add Tests of Model

---
+++ NewsApp/NewsAppModel/NewsAppModel.h
@@ -0,0 +1,19 @@
+//
+//  NewsAppModel.h
+//  NewsAppModel
+//
+//  Created by TanakaJun on 2016/01/22.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewsAppModel.
+FOUNDATION_EXPORT double NewsAppModelVersionNumber;
+
+//! Project version string for NewsAppModel.
+FOUNDATION_EXPORT const unsigned char NewsAppModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewsAppModel/PublicHeader.h>
+
+
