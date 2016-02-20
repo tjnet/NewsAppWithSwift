44a5b402f2ea9a3b7a841d221548d01bac10d9ee
tjnet <rsa14631@nifty.com>
2016-02-20 19:20:40 +0900

    create FeedResponseSpec

---
+++ NewsApp/NewsAppModelTests/FeedResponseSpec.swift
@@ -0,0 +1,32 @@
+//
+//  FeedResponseSpec.swift
+//  NewsApp
+//
+//  Created by TanakaJun on 2016/02/20.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import Mockingjay
+
+@testable import NewsAppModel
+
+
+class FeedResponseSpec: QuickSpec {
+    override func spec() {
+        beforeEach {
+            
+        }
+        
+        describe("JSON") {
+            it("eventually gets JSON data as specified with parameters.") {
+                
+            }
+            it("eventually gets an error if the network has a problem.") {
+                
+            }
+        }
+    }
+}
