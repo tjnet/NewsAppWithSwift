95a4e39d93ec242ca7793115cbdf9d052a8097a4
tjnet <rsa14631@nifty.com>
2016-02-20 14:16:01 +0900

    add ArticleTableViewCell

---
+++ NewsApp/NewsAppView/ArticleTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsApp
+//
+//  Created by TanakaJun on 2016/02/20.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import UIKit
+
+internal final class ArticleTableViewCell: UITableViewCell {
+    
+}
