b3808f9d0f6b68e72a73daa8f2d529580cc5aa92
tjnet <rsa14631@nifty.com>
2016-02-20 14:26:59 +0900

    add some ViewModel

---
+++ NewsApp/NewsAppViewModel/ArticleTableViewCellModeling.swift
@@ -0,0 +1,12 @@
+//
+//  ArticleTableViewCellModeling.swift
+//  NewsApp
+//
+//  Created by TanakaJun on 2016/02/20.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+public protocol ArticleTableViewCellModeling {
+    
+}
+
---
+++ NewsApp/NewsAppViewModel/ArticleTableViewModeling.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleTableViewModeling.swift
+//  NewsApp
+//
+//  Created by TanakaJun on 2016/02/20.
+//  Copyright © 2016年 edu.self. All rights reserved.
+//
+
+import Foundation
+
+public protocol ArticleTableViewModeling {
+    
+}
